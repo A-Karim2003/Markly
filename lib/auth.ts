162cc00297e147aee248d95652489695a485a0a3
Abdoulkarim <17a.karim@gmail.com>
2026-05-01 15:47:28 +0100

    feat: Add nextCookies plugin for Server Action support. Add proxy-based route protection for authenticated users

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,14 @@
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
   database: new Pool({
     connectionString: process.env.DATABASE_URL,
   }),
+
+  // When Better Auth tries to set cookies inside a Server Action, manually sync those cookies into Next.js/browser.”
+  plugins: [nextCookies()],
 
   emailAndPassword: {
     enabled: true,
