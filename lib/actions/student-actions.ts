17571323da0b3294b1de5a190159a5b97bd3cddb
Abdoulkarim <17a.karim@gmail.com>
2026-05-25 19:40:16 +0100

    feat: Automate assessment creation on module enrollment

--- lib/actions/student-actions.ts
+++ lib/actions/student-actions.ts
@@ -6,7 +6,7 @@ import { getSession } from "./auth-actions";
 import { getStudentProfile } from "../data/student-profiles";
 
 export async function updateStudentYear(studentId: number, year: number) {
-  const session = getSession();
+  const session = await getSession();
   if (!session) throw new Error("User not authenticated");
 
   const supabase = await createClient();
@@ -25,7 +25,7 @@ export async function updateStudentOnboardingStep(
   studentId: number,
   step: number,
 ) {
-  const session = getSession();
+  const session = await getSession();
   if (!session) throw new Error("User not authenticated");
 
   const supabase = await createClient();
@@ -43,22 +43,59 @@ export async function enrolStudentModules(
   studentId: number,
   moduleIds: number[],
 ) {
-  const session = getSession();
+  const session = await getSession();
   if (!session) throw new Error("User not authenticated");
 
   const studentProfile = await getStudentProfile();
-
   const supabase = await createClient();
 
-  const rows = moduleIds.map((moduleId) => ({
-    student_profile_id: studentId,
-    module_id: moduleId,
-    year: studentProfile.year!,
-  }));
-
-  const { error } = await supabase.from("student_modules").insert(rows);
+  const { data: studentModules, error } = await supabase
+    .from("student_modules")
+    .insert(
+      moduleIds.map((moduleId) => ({
+        student_profile_id: studentId,
+        module_id: moduleId,
+        year: studentProfile.year!,
+      })),
+    )
+    .select("id, module_id");
 
   if (error) throw new Error(error.message);
+
+  const { data: modules, error: modulesError } = await supabase
+    .from("modules")
+    .select("id, module_assessments_scheme(id, name, weight)")
+    .in("id", moduleIds);
+
+  if (modulesError) throw new Error(modulesError.message);
+
+  const assessmentsToSeed = modules.flatMap((module) => {
+    // For this module, find its corresponding student_module row so I can get the student_module_id
+    const studentModule = studentModules.find(
+      (studentModule) => studentModule.module_id === module.id,
+    );
+    if (!studentModule) return [];
+
+    // For each scheme row, create as assessment row for the corresponding module
+    return module.module_assessments_scheme.map((scheme) => ({
+      name: scheme.name,
+      weight: scheme.weight,
+      grade: null,
+      scheme_id: scheme.id,
+      student_module_id: studentModule.id,
+    }));
+  });
+
+  if (assessmentsToSeed.length) {
+    const { error: assessmentsError } = await supabase
+      .from("assessments")
+      .insert(assessmentsToSeed);
+
+    if (assessmentsError) throw new Error(assessmentsError.message);
+  }
+
+  revalidatePath("/dashboard");
+  revalidatePath("/modules");
 }
 
 type UpdateStudentProfileData = {
